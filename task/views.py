16a6b5976366ad7bc2708db9d8a38ae206f20d7d
asingjr2 <asingjr2@gmail.com>
2018-07-28 19:51:17 -0400

    changing back

--- task/views.py
+++ task/views.py
@@ -98,7 +98,7 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
 
     # If valid form work correctly...need something if it doesn't
     def form_valid(self, form): 
-        form.instance.creator = self.request.user.id
+        form.instance.creator = self.request.user
         self.object = form.save()
         return redirect("/home")
 
